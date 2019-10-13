2e01e79246d8374eb8aa18e2f5a9f0de42bd8437
Markus Burkert <gebolze@gmail.com>
2019-10-13 22:05:52 +0200

    Moved SingleInstance logic to separate namespace

---
+++ WpfApp1/SingleInstance/ISingleInstanceApp.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Microsoft.Shell
+{
+    public interface ISingleInstanceApp
+    {
+        bool SignalExternalCommandLineArgs(IList<string> args);
+    }
+}
